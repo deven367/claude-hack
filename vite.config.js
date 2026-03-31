c85e9f684da788ca2025d56e00f6e8a30d3f471e
deven367 <masterdeven@gmail.com>
2026-03-31 12:36:40 -0400

    try again

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/share-your-story-frontend/',
+  base: './',
   plugins: [react()],
   server: {
     proxy: {
